e0f10f463074050506ef3816ffc87c0a7ae3c28b
Tejasbisen786 <bisentejas@gmail.com>
2023-07-12 21:42:34 +0530

    day 3

---
+++ bootcampDay-3.py
@@ -0,0 +1,86 @@
+# ********* Day-3 :  Python Bootcamp **********
+# break and continue 
+
+
+
+#Function In Python 
+
+# def bolohii(count):   # Function Defination 
+#     print("Hiii Folks",count)
+
+# count=76
+# bolohii(count)   #  Function Call 
+
+
+# # Return Type 
+
+# def getnum(x,y):
+#     sum = x+y
+#     return sum,x
+# x=9
+# y=10
+# res=getnum(x,y)
+
+# print("Sum is :",res)
+
+# x=int(input("ENter The Two Number"))
+# def getnum(x):
+#     newNum=x
+#     return x
+# res=getnum(x)
+# print("Num is:",res)
+
+#Swap A Numner Using Temp Or not using temp
+# a=6
+# b=8
+# print(a)
+# print(b)
+# c=a
+# a=b
+# b=c
+# a,b,c=12,12,8
+# print(a,b,c)
+# print(b)
+
+
+# List 
+
+apna_list=["ab","bc","cd",22,33]
+# apna_list1 = apna_list[:1]
+# print(apna_list1)
+# print(apna_list)
+
+# print(apna_list[3])
+
+#*****************Tuple************** 
+#  ordered | unchanged 
+apna_list=("ab","bc","cd",22,33)
+
+# print(apna_list)
+apna_list= apna_list[:1]  #slicing || 
+# print(apna_list)
+# apna_list[1]="pqr"   # Not Chnaged Value  canot re assign
+# print(apna_list)
+
+
+
+# tuple to list conversion 
+
+apna_tuple=("ab","bc","cd",22,33)
+apna_tuple1=list(apna_tuple)
+apna_tuple1[1]="Tushar"
+newtuple= tuple(apna_tuple1)
+print(newtuple)
+print(apna_tuple1)
+
+
+
+
+
+
+
+
+
+
+
+
