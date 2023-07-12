ebdeee4f9ccd25c59dd7c8e25c1e6c0ee0c5d9f9
Tejasbisen786 <bisentejas@gmail.com>
2023-07-12 21:46:38 +0530

    file added day 3

--- day2Assignment.py
+++ day2Assignment.py
@@ -2,10 +2,16 @@
 # According to the time Greeting in class.
 
 # Define Variable
-time= int(input("enter your current time : "))
-if time >= 0 and current_time < 12:
-    print("Good morning")
-elif time >= 12 and current_time < 16:
-    print("Good afternoon")
-else:
-    print("Good night")
+# time= int(input("enter your current time : "))
+# if time >= 0 and current_time < 12:
+#     print("Good morning")
+# elif time >= 12 and current_time < 16:
+#     print("Good afternoon")
+# else:
+#     print("Good night")
+ 
+
+# taking user Input And Print The Number
+# num = int (input("Enter The Number"))
+# for num in range(1,num,1):
+#     print(num)
---
+++ day3Assignment.py
@@ -0,0 +1,11 @@
+
+# a=6
+# b=8
+# print(a)
+# print(b)
+# c=a
+# a=b
+# b=c
+# a,b,c=12,12,8
+# print(a,b,c)
+# print(b)
