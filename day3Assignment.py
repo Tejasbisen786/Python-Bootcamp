d8cf5b41fbf8e265ab6570b6daab9e1afb560984
Tejasbisen786 <bisentejas@gmail.com>
2023-07-13 19:39:32 +0530

    assgnment updated

--- day3Assignment.py
+++ day3Assignment.py
@@ -1,4 +1,12 @@
 
+
+# *******assignment *********
+# heros=["Tejas","Tushar","Bisen","Gidhadi","Gondia","Amravati"]
+# for i in range(len(heros)):
+    # print(i,heros[i])   # Printing Value Of heros with indexing
+
+
+# Swap Value 
 # a=6
 # b=8
 # print(a)
@@ -9,3 +17,29 @@
 # a,b,c=12,12,8
 # print(a,b,c)
 # print(b)
+
+
+
+# marks=["1","2","4","6","4","6","5"]
+# print(marks)
+# print(marks[:4])  #slicing
+# print(marks[4:])  #slicing
+
+# for i in range(0,8,2):
+#  print(i)
+# heros=["Tejas","Tushar","Bisen","Gidhadi","Gondia","Amravati"]
+# print(heros)
+
+# for i in range(len(heros)):
+    # print(i,heros[i])   # Printing Value Of heros with indexing
+
+
+
+# ***********Tuple ***********
+#Ordered Unchanged 
+# heros=("Tejas","Tushar","Bisen","Gidhadi","Gondia","Amravati")
+# # print(heros)
+# heros1=list(heros)
+# heros1[1]="Harsh"
+# print(heros1)
+# # print(heros)
