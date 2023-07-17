7a11f551a00d22cb6b1cc1429feaa320a2e9b4ab
Tejasbisen786 <bisentejas@gmail.com>
2023-07-17 23:13:06 +0530

    day 6 practise done

---
+++ day6Assignment.py
@@ -0,0 +1,42 @@
+# Abstraction and Encapsulation 
+
+# class Person :
+#     def __init__(self,name,age,prof):
+#         self.name= name
+#         self.age= age
+#         self.prof= prof
+
+#         def bioData(self):
+#             print(self.name)
+
+
+#             obj1 = Person("Tejas",21,"Programmer")
+#             obj1.bioData
+
+
+
+
+
+# Calculator Program using Procedural Programming 
+
+# def getAdd(num ,num1):
+#     return num+num1
+# def getSub(num ,num1):
+#     return num-num1
+# def getMul(num ,num1):
+#     return num*num1
+# def getDiv(num ,num1):
+#     return num/num1
+
+# num = int(input("Enter The First Numbe: r"))
+# num1= int(input("Enter The Second Number: "))
+# res= getAdd(num,num1)
+# print("Addition is:",res)
+# res= getSub(num,num1)
+# print("Subtraction is: ",res)
+# res= getSub(num,num1)
+# print("Division is: ",res)
+# res= getMul(num,num1)
+# print("Multiplication is: ",res)
+
+# Polymorphism And Inheritance 
